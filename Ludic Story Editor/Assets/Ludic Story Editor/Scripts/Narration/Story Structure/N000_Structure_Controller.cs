f783198eef6c7443f64b29a2c05d3de123549cde
JonasZimmer <Zimmer-Jonas@web.de>
2016-03-08 21:53:42 +0100

    Struktur Controller

---
+++ Ludic Story Editor/Assets/Ludic Story Editor/Scripts/Narration/Story Structure/N000_Structure_Controller.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace LudicStoryEditor.NARRATION
+{
+    /*
+     * Narration Basis Klasse N000
+     * Struktur Controller
+     * Zuständig für Laden/Speichern der Story Struktur
+     * Interpretiert die importierten Daten und konstruiert daraus die Struktur
+     */
+    public sealed class N000_Structure_Controller
+    {
+        private N000_Structure_Controller instance;
+        public N000_Structure_Controller Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new N000_Structure_Controller();
+                return instance;
+            }
+        }
+
+        //Erstellt die Story Struktur basierend auf den importierten Daten
+        public void Initialisieren()
+        {
+
+        }
+
+        //Speichert die Struktur in eine XML
+        public void Speichern()
+        {
+
+        }
+
+        //Lädt die Struktur aus der gespeicherten XML
+        public void Laden()
+        {
+
+        }
+    }
+}
